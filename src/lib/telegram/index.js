a8c595983e0aaa2f99c00d1550c1a782cd1ca712
Kenshin <fxblog@gmail.com>
2024-11-08 11:12:43 +0800

    Add 'origin_content' workflow.

--- src/lib/telegram/index.js
+++ src/lib/telegram/index.js
@@ -157,7 +157,7 @@ function getPost($, item, { channel, staticProxy, index = 0, type }) {
     $(a)?.attr('href', `/search/${encodeURIComponent($(a)?.text())}`)
   })?.map((_index, a) => $(a)?.text()?.replace('#', ''))?.get()
 
-  const origin_content = content?.html()?.replace(/href="\/search\//gi, `href="https://n.simp.red/search/`)
+  const origin_content = content?.html()?.replace(/href="\/search\//gi, `href="n.simp.red/search/`)
 
   if (type === 'post') {
     const str = content?.html()
